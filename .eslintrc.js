32aefb7a3510a95fa5b0934817ff767a4cb83210
Tomas Echeverri Valencia <tomechval@gmail.com>
2020-11-26 17:18:25 +0000

    Configure ESLint to work with Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,13 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: "eslint:recommended",
+  extends: ["eslint:recommended", "prettier"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  rules: {},
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
 };
